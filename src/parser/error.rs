cc82cf950e944967fa89a69be251dab3b46087e8
Alexander Rafferty <hello@alexanderrafferty.com>
2026-04-28 12:32:52 +1000

    Add pipe operator; add line_index

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,17 +1,20 @@
 use super::span::Span;
+use line_index::LineIndex;
+use std::sync::Arc;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub struct ParseError {
     pub message: String,
     pub span: Span,
+    pub line_index: Arc<LineIndex>,
 }
 
 pub type Result<T, E = ParseError> = std::result::Result<T, E>;
 
 impl std::fmt::Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // FIXME: span
-        write!(f, "Parse error at {:?}: {}", self.span, self.message)
+        let line_col = self.line_index.line_col(self.span.start());
+        write!(f, "Parse error at {:?}: {}", line_col, self.message)
     }
 }
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,14 +1,40 @@
-use super::error::Result;
+use super::error::{ParseError, Result};
 use super::lexer::{Lexer, Token};
+use line_index::{LineIndex, TextRange, TextSize};
+use std::cell::OnceCell;
+use std::sync::Arc;
 
 pub struct Parser<'a> {
+    src: &'a str,
     lexer: Lexer<'a>,
     previous: Token<'a>,
     current: Token<'a>,
+    line_index: OnceCell<Arc<LineIndex>>,
 }
 
 impl<'a> Parser<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Self {
+            src,
+            lexer: Lexer::new(src),
+            previous: Token::default(),
+            current: Token::default(),
+            line_index: OnceCell::new(),
+        }
+    }
+
     pub fn parse_expr(&mut self) -> Result<()> {
-        Ok(())
+        Err(self.error(
+            "not implemented",
+            TextRange::new(TextSize::new(0), TextSize::new(0)),
+        ))
+    }
+
+    fn error(&self, msg: impl Into<String>, span: TextRange) -> ParseError {
+        let line_index = self
+            .line_index
+            .get_or_init(|| Arc::new(line_index::LineIndex::new(self.src)))
+            .clone();
+        ParseError { message: msg.into(), span, line_index }
     }
 }
