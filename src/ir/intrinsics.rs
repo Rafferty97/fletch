9eeead3f170ca6e53ceb1f7b407bbcae83bb1f7b
Alexander Rafferty <hello@alexanderrafferty.com>
2026-05-05 22:43:16 +1000

    All basic math intrinsics

--- src/ir/intrinsics.rs
+++ src/ir/intrinsics.rs
@@ -1,13 +1,45 @@
 use super::VarId;
 
-pub const ADD_I32: VarId = VarId(0);
-pub const ADD_U64: VarId = VarId(1);
+pub const ADD_I8: VarId = VarId(0);
+pub const ADD_I16: VarId = VarId(1);
+pub const ADD_I32: VarId = VarId(2);
+pub const ADD_I64: VarId = VarId(3);
+pub const ADD_U8: VarId = VarId(4);
+pub const ADD_U16: VarId = VarId(5);
+pub const ADD_U32: VarId = VarId(6);
+pub const ADD_U64: VarId = VarId(7);
+pub const ADD_F32: VarId = VarId(8);
+pub const ADD_F64: VarId = VarId(9);
 
-pub const SUB_I32: VarId = VarId(10);
-pub const SUB_U64: VarId = VarId(11);
+pub const SUB_I8: VarId = VarId(10);
+pub const SUB_I16: VarId = VarId(11);
+pub const SUB_I32: VarId = VarId(12);
+pub const SUB_I64: VarId = VarId(13);
+pub const SUB_U8: VarId = VarId(14);
+pub const SUB_U16: VarId = VarId(15);
+pub const SUB_U32: VarId = VarId(16);
+pub const SUB_U64: VarId = VarId(17);
+pub const SUB_F32: VarId = VarId(18);
+pub const SUB_F64: VarId = VarId(19);
 
-pub const MUL_I32: VarId = VarId(20);
-pub const MUL_U64: VarId = VarId(21);
+pub const MUL_I8: VarId = VarId(20);
+pub const MUL_I16: VarId = VarId(21);
+pub const MUL_I32: VarId = VarId(22);
+pub const MUL_I64: VarId = VarId(23);
+pub const MUL_U8: VarId = VarId(24);
+pub const MUL_U16: VarId = VarId(25);
+pub const MUL_U32: VarId = VarId(26);
+pub const MUL_U64: VarId = VarId(27);
+pub const MUL_F32: VarId = VarId(28);
+pub const MUL_F64: VarId = VarId(29);
 
-pub const DIV_I32: VarId = VarId(30);
-pub const DIV_U64: VarId = VarId(31);
+pub const DIV_I8: VarId = VarId(30);
+pub const DIV_I16: VarId = VarId(31);
+pub const DIV_I32: VarId = VarId(32);
+pub const DIV_I64: VarId = VarId(33);
+pub const DIV_U8: VarId = VarId(34);
+pub const DIV_U16: VarId = VarId(35);
+pub const DIV_U32: VarId = VarId(36);
+pub const DIV_U64: VarId = VarId(37);
+pub const DIV_F32: VarId = VarId(38);
+pub const DIV_F64: VarId = VarId(39);
