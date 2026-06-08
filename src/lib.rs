a86f63e6d81d1d8b14fd8035f624aebbfa4e9f6b
Alexander Rafferty <alexanderrafferty@Alexanders-MacBook-Pro.local>
2026-06-08 23:49:53 +1000

    add code from Claude

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 // Type
 
+use std::collections::HashMap;
+
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub enum Ty {
     Never,
@@ -82,8 +84,8 @@ pub fn variance_in_ty(ty: &Ty, var: VarId) -> Variance {
     match ty {
         Ty::Tuple(tys) => variance_in_tys(tys, var),
         Ty::Func(func) => {
-            let param_variance = variance_in_tys(&func.params, var);
-            let ret_variance = variance_in_ty(&func.ret, var).invert();
+            let param_variance = variance_in_tys(&func.params, var).invert();
+            let ret_variance = variance_in_ty(&func.ret, var);
             Variance::merge(param_variance, ret_variance)
         }
         Ty::Var(v) if *v == var => Variance::Covariant,
@@ -103,6 +105,7 @@ pub fn meet(a: &Ty, b: &Ty) -> Ty {
     match (a, b) {
         _ if a == b => a.clone(),
         (Ty::Err, _) | (_, Ty::Err) => Ty::Err,
+        (ty, Ty::Top) | (Ty::Top, ty) => ty.clone(),
         (Ty::Int(a), Ty::Int(b)) => Ty::Int(*a.min(b)),
         (Ty::Nullable(a), Ty::Nullable(b)) => Ty::Nullable(meet(a, b).into()),
         (a, Ty::Nullable(b)) => meet(a, b),
@@ -116,10 +119,11 @@ pub fn join(a: &Ty, b: &Ty) -> Ty {
     match (a, b) {
         _ if a == b => a.clone(),
         (Ty::Err, _) | (_, Ty::Err) => Ty::Err,
+        (ty, Ty::Never) | (Ty::Never, ty) => ty.clone(),
         (Ty::Int(a), Ty::Int(b)) => Ty::Int(*a.max(b)),
         (Ty::Nullable(a), Ty::Nullable(b)) => Ty::Nullable(join(a, b).into()),
         (a, Ty::Nullable(b)) => Ty::Nullable(join(a, b).into()),
-        (Ty::Nullable(a), b) => Ty::Nullable(join(a, b).into()).into(),
+        (Ty::Nullable(a), b) => Ty::Nullable(join(a, b).into()),
         // todo: tuple, func
         _ => Ty::Top,
     }
@@ -222,13 +226,58 @@ impl InferCtx {
         }
     }
 
-    pub fn solve_var(&self, var: VarId, ret_ty: &Ty) -> Ty {
-        // variance-directed bound selection
-        todo!()
+    pub fn solve_var(&mut self, var: VarId, ret_ty: &Ty) -> Ty {
+        self.solve_var_with_variance(var, variance_in_ty(ret_ty, var))
     }
 
-    pub fn solve_ty(&self, ty: &Ty, ret_ty: &Ty) -> Ty {
-        // substitute all vars recursively
-        todo!()
+    pub fn solve_ty(&mut self, ty: &Ty, ret_ty: &Ty) -> Ty {
+        let variances: HashMap<VarId, Variance> = (0..self.vars.len())
+            .map(|i| {
+                let id = VarId(i as u32);
+                (id, variance_in_ty(ret_ty, id))
+            })
+            .collect();
+        self.solve_ty_inner(ty, &variances)
+    }
+
+    fn solve_ty_inner(&mut self, ty: &Ty, variances: &HashMap<VarId, Variance>) -> Ty {
+        match ty {
+            Ty::Var(id) => self.solve_var_with_variance(*id, variances[id]),
+            Ty::Nullable(inner) => Ty::Nullable(self.solve_ty_inner(inner, variances).into()),
+            Ty::Tuple(tys) => Ty::Tuple(
+                tys.iter()
+                    .map(|t| self.solve_ty_inner(t, variances))
+                    .collect(),
+            ),
+            Ty::Func(f) => Ty::Func(FuncTy {
+                params: f
+                    .params
+                    .iter()
+                    .map(|p| self.solve_ty_inner(p, variances))
+                    .collect(),
+                ret: self.solve_ty_inner(&f.ret, variances).into(),
+            }),
+            _ => ty.clone(),
+        }
+    }
+
+    fn solve_var_with_variance(&mut self, var: VarId, variance: Variance) -> Ty {
+        let v = &self.vars[var.0 as usize];
+        if !check_subtype(&v.lower, &v.upper) {
+            self.errors
+                .push(format!("cannot assign {:?} to {:?}", v.lower, v.upper));
+            return Ty::Err;
+        }
+        match variance {
+            Variance::Covariant | Variance::Bivariant => v.lower.clone(),
+            Variance::Contravariant => v.upper.clone(),
+            Variance::Invariant => {
+                if v.lower == v.upper {
+                    v.lower.clone()
+                } else {
+                    Ty::Err
+                }
+            }
+        }
     }
 }
